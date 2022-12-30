2a65eb6349010712dbf5d8d40e1fe6f37be58748
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-12-30 02:08:31 -0700

    Eliminating redundant code and preparing foreach loop

--- 2.R_Code/1.Scripts/1.DataCleaning/NDVI_downloand.R
+++ 2.R_Code/1.Scripts/1.DataCleaning/NDVI_downloand.R
@@ -1,3 +1,15 @@
+#   Script Details                                                          ####
+
+# Author: Waldemar Ortiz-Calo
+
+# Date:2022-12-29 
+
+# Purpose: 
+
+###############################################################################
+#   Library / Functions / Data                                              ####
+
+#      Library                                                              ####
 library(MODISTools)
 library(terra)
 library(raster)
@@ -5,16 +17,21 @@ library(tidyverse)
 library(stringr)
 library(foreach)
 library(doParallel)
+library(sf)
 
+#      Functions                                                            ####
+
+#      Data                                                                 ####
+shp_montana <- st_read("1.Data\\data_clean\\MontanaBoundaries\\MontanaCountyBoundaries.shp") %>% 
+  vect()
+###############################################################################
+#   NDVI Download                                                           ####
+#      NDVI Details                                                         ####
 products <- mt_products()
-head(products)
-
-
 bands <- mt_bands(product = "VNP13A1")
-head(bands)
-
 dates <- mt_dates(product = "VNP13A1", lat = 46.8797, lon = -110.3626)
-head(dates)
+
+#      Extent Information                                                   ####
 
 lat_list <- seq(from = 44, to = 49,by = 1)
 lon_list <- seq(from = -116, to = -104,by = 1)
@@ -42,7 +59,7 @@ writeRaster(montana_ndvi,
               "_t",
               str_pad(1, 3, pad = "0"),
               ".tif"))
-#      [For loop version]                                                              ####
+#      [For loop version]                                                   ####
 for (i in 1:nrow(coord_grid)) {
   
   # Data downloader
@@ -74,7 +91,7 @@ for (i in 1:nrow(coord_grid)) {
   # Iteration Tracker
   print(paste0(i, " out of ", nrow(coord_grid), " completed"))
 }
-#      [Foreach Version]                                                              ####
+#      [Foreach Version]                                                    ####
 
 # Cluster Number
 cl <- makeCluster(4)
@@ -93,116 +110,74 @@ clusterEvalQ(cl,
 # Exporting data to clusters
 clusterExport(cl=cl, varlist=c("coord_grid","dates"), envir=environment())
 
-foreach(i = 1:nrow(coord_grid),
-        .combine = c,
-        .errorhandling = "pass") %dopar% 
-  {
-    # Data downloader
-    montana_ndvi<- mt_subset(product = "VNP13A1",
-                             lat = coord_grid[i,1],
-                             lon =  coord_grid[i,2],
-                             band = "500_m_16_days_NDVI",
-                             start = dates[1,2],
-                             end = dates[1,2],
-                             km_lr = 50,
-                             km_ab = 100,
-                             site_name = paste0("MT_NDVI_",dates[1,2]),
-                             internal = T,
-                             progress = F) %>% 
-      mt_to_raster(reproject = T) %>% 
-      rast() 
-    
-    # Raster Export
-    writeRaster(montana_ndvi,
-                filename = paste0(
-                  "1.Data/data_raw/NDVI/",
-                  "MT_NDVI_",
-                  dates[1,2],
-                  "_t",
-                  str_pad(i, 3, pad = "0"),
-                  ".tif"),
-                overwrite=TRUE)
-    
-    # Iteration Tracker
-    return(paste0(i, " out of ", nrow(coord_grid), " completed"))
-  }
-
+foreach (t = 1:nrow(dates),
+         .combine = c,
+         .errorhandling = "pass") %do% {
+           
+           # Subsetting the date for the NDVI 
+           date <- dates[t,2]
+           
+           # Foreach loop to download all tiles for that date
+           foreach(i = 1:nrow(coord_grid),
+                   .combine = c,
+                   .errorhandling = "pass") %dopar% 
+             {
+               # Data downloader
+               montana_ndvi<- mt_subset(product = "VNP13A1",
+                                        lat = coord_grid[i,1],
+                                        lon =  coord_grid[i,2],
+                                        band = "500_m_16_days_NDVI",
+                                        start = date,
+                                        end = date,
+                                        km_lr = 50,
+                                        km_ab = 100,
+                                        site_name = paste0("MT_NDVI_",date),
+                                        internal = T,
+                                        progress = F) %>% 
+                 mt_to_raster(reproject = T) %>% 
+                 rast() 
+               
+               # Raster Export
+               writeRaster(montana_ndvi,
+                           filename = paste0(
+                             "1.Data/data_raw/NDVI/",
+                             "MT_NDVI_",
+                             date,
+                             "_t",
+                             str_pad(i, 3, pad = "0"),
+                             ".tif"),
+                           overwrite=TRUE)
+               
+               # Iteration 
+               return(i)
+             }
+           
+           # Making a list of the raster tiles
+           tiles <- list.files("1.Data/data_raw/NDVI",
+                               full.names = T,
+                               pattern = "t\\d\\d\\d.tif") %>% 
+             str_subset(pattern = date)
+           
+           # Compiling Tiles to raster
+           tile_ras <- vrt(tiles, "test.vrt", 
+                           overwrite=TRUE)
+           
+           # Final NDVI map for Montana and date
+           final <- mask(tile_ras,shp_montana) 
+           
+           # Exporting Raster
+           writeRaster(x = final,
+                       filename = paste0("1.Data/data_clean/NDVI/NDVI_",
+                                         date,
+                                         ".tif"),
+                       overwrite = T)
+           
+           # Deleting raw tiles
+           file.remove(tiles)
+           
+           # Check
+           return(paste0(t, " out of ", nrow(dates), " completed"))
+         }
 
 
 ###############################################################################
-
-montana_ndvi1 <- mt_subset(product = "VNP13A1",
-                         lat = 44.0,
-                         lon =  -116,
-                         band = "500_m_16_days_NDVI",
-                         start = dates[1,2],
-                         end = dates[1,2],
-                         km_lr = 50,
-                         km_ab = 100,
-                         site_name = "MT_NDVI",
-                         out_dir = "1.Data/data_raw/NDVI",
-                         internal = T,
-                         progress = T)
-
-montana_ndvi2 <- mt_subset(product = "VNP13A1",
-                           lat = 44.0,
-                           lon =  -115,
-                           band = "500_m_16_days_NDVI",
-                           start = dates[1,2],
-                           end = dates[1,2],
-                           km_lr = 50,
-                           km_ab = 100,
-                           site_name = "MT_NDVI",
-                           out_dir = "1.Data/data_raw/NDVI",
-                           internal = T,
-                           progress = T)
-
-montana_ndvi3 <- mt_subset(product = "VNP13A1",
-                           lat = 45.0,
-                           lon =  -116,
-                           band = "500_m_16_days_NDVI",
-                           start = dates[1,2],
-                           end = dates[1,2],
-                           km_lr = 50,
-                           km_ab = 100,
-                           site_name = "MT_NDVI",
-                           out_dir = "1.Data/data_raw/NDVI",
-                           internal = T,
-                           progress = T)
-
-montana_ndvi4 <- mt_subset(product = "VNP13A1",
-                           lat = 45.0,
-                           lon =  -115,
-                           band = "500_m_16_days_NDVI",
-                           start = dates[1,2],
-                           end = dates[1,2],
-                           km_lr = 50,
-                           km_ab = 100,
-                           site_name = "MT_NDVI",
-                           out_dir = "1.Data/data_raw/NDVI",
-                           internal = T,
-                           progress = T)
-
-
-ras1 <- mt_to_raster(montana_ndvi1,
-                    reproject = T)
-
-ras2 <- mt_to_raster(montana_ndvi2,
-                    reproject = T)
-
-ras3 <- mt_to_raster(montana_ndvi3,
-                     reproject = T)
-
-ras4 <- mt_to_raster(montana_ndvi4,
-                     reproject = T)
-
-r1 <- ras1 %>% rast() %>% raster()
-r2 <- ras2 %>% rast() %>% raster()
-r3 <- ras3 %>% rast() %>% raster()
-r4 <- ras4 %>% rast() %>% raster()
-
-
-mapview::mapview(r1) + mapview::mapview(r2) +
-  mapview::mapview(r3) + mapview::mapview(r4)
-
-coord_grid[1:2,]
