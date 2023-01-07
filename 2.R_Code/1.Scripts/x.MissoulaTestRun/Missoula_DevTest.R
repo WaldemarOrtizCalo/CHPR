ca6312947e0b13c91f0690cd1777e18c47ab6b17
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-01-06 21:52:22 -0700

    Cleaning Code

--- 2.R_Code/1.Scripts/x.MissoulaTestRun/Missoula_DevTest.R
+++ 2.R_Code/1.Scripts/x.MissoulaTestRun/Missoula_DevTest.R
@@ -41,18 +41,15 @@ missoula_cadastral <- read_sf("1.Data\\data_clean\\MontanaCadastral\\MontanaCada
 ###############################################################################
 #  Road Buffer Layer Creation and Export                                    ####
 
-#      Creating Buffered Layer and Export                                   ####
+#      Creating Buffered Layer                                              ####
 
-# Creating a 20m buffer 
-roads_buffered <- missoula_roads %>% st_buffer(20)
+# Creating a 50m buffer 
+roads_buffered <- missoula_roads %>% st_buffer(50)
 
 # Exporting
 st_write(obj = roads_buffered,
          dsn = "1.Data/data_clean/RoadLayer/road_buffers.shp",
          append = F)
-
-# Cleaning Environment
-rm(roads_buffered)
 
 #      Creating and Exporting Maps                                          ####
 
@@ -109,59 +106,114 @@ GAP2 <- protected_areas %>%  filter(GAP_Sts == "2")
 GAP3 <- protected_areas %>%  filter(GAP_Sts == "3")
 GAP4 <- protected_areas %>%  filter(GAP_Sts == "4")
 
+#        [Eliminating WITHIN Statuses]                                      ####
+
+overlaps1 <- st_intersection(GAP1) %>% mutate(n.overlaps = as.character(n.overlaps))
+overlaps2 <- st_intersection(GAP2) %>% mutate(n.overlaps = as.character(n.overlaps))
+overlaps3 <- st_intersection(GAP3) %>% mutate(n.overlaps = as.character(n.overlaps))
+overlaps4 <- st_intersection(GAP4) %>% mutate(n.overlaps = as.character(n.overlaps))
+
+p1 <- ggplot(overlaps1)+
+  geom_sf(data = missoula_boundary) +
+  geom_sf(aes(fill = n.overlaps)) +
+  theme_bw()+
+  ggtitle("Overlapping Polygons GAP Status 1")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_x_continuous(breaks = round(seq(st_bbox(protected_areas)[[1]],st_bbox(protected_areas)[[3]],by = 0.5),digits = 2))
+
+p2 <- ggplot(overlaps2)+
+  geom_sf(data = missoula_boundary) +
+  geom_sf(aes(fill = n.overlaps)) +
+  theme_bw()+
+  ggtitle("Overlapping Polygons GAP Status 2")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_x_continuous(breaks = round(seq(st_bbox(protected_areas)[[1]],st_bbox(protected_areas)[[3]],by = 0.5),digits = 2))
+
+p3 <- ggplot(overlaps3)+
+  geom_sf(data = missoula_boundary) +
+  geom_sf(aes(fill = n.overlaps)) +
+  theme_bw()+
+  ggtitle("Overlapping Polygons GAP Status 3")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_x_continuous(breaks = round(seq(st_bbox(protected_areas)[[1]],st_bbox(protected_areas)[[3]],by = 0.5),digits = 2))
+
+p4 <- ggplot(overlaps4)+
+  geom_sf(data = missoula_boundary) +
+  geom_sf(aes(fill = n.overlaps)) +
+  theme_bw()+
+  ggtitle("Overlapping Polygons GAP Status 4")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_x_continuous(breaks = round(seq(st_bbox(protected_areas)[[1]],st_bbox(protected_areas)[[3]],by = 0.5),digits = 2))
+
+ggarrange(p1,p2,p3,p4,
+          ncol = 2,nrow = 2)
+
 #        [Eliminating BETWEEN Statuses]                                     ####
 
-# Preparing Cleaning Layers
-GAP2_cleaner <- protected_areas %>% filter(GAP_Sts %in% c("1")) %>%  st_combine() %>% st_make_valid()
-GAP3_cleaner <- protected_areas %>% filter(GAP_Sts %in% c("1","2")) %>% st_combine() %>% st_make_valid()
-GAP4_cleaner <- protected_areas %>% filter(GAP_Sts %in% c("1","2","3")) %>% st_combine() %>% st_make_valid()
+mapview(GAP1, col.regions = "red") + 
+  mapview(GAP2, col.regions = "blue") +
+  mapview(GAP3, col.regions = "green") +
+  mapview(GAP4, col.regions = "yellow")
 
-# Setting s2 use to FALSE
-sf_use_s2(F)
 
-# Creating Differrence Layers 
-GAP2_clean <- st_difference(GAP2,GAP2_cleaner)
-GAP3_clean <- st_difference(GAP3,GAP3_cleaner)
-GAP4_clean <- st_difference(GAP4,GAP4_cleaner)
+out.overlap <- st_overlaps(GAP1, GAP3)
 
-# Joining Everything into one final layer
-GAP_clean <- bind_rows(GAP1,
-                   GAP2_clean,
-                   GAP3_clean,
-                   GAP4_clean)
+dif1 <- st_intersection(GAP1, GAP3)
+dif2 <- st_intersection(GAP3, GAP1)
 
-# Map
-#mapview(GAP_clean, col.regions = "green")+ 
-# mapview(GAP1, col.regions = "red")+
-#  mapview(GAP2, col.regions = "purple")+
-#  mapview(GAP3, col.regions = "blue")+
-#  mapview(GAP4,col.regions = "yellow")
+g1_big <- st_union(GAP1)
+g3_big <- st_union(GAP3)
 
-#        Cleaning Environment                                               ####
+st_erase = function(x, y) st_difference(x, st_union(st_combine(y)))
 
-rm(GAP2_clean)
-rm(GAP3_clean)
-rm(GAP4_clean)
-rm(GAP2_cleaner)
-rm(GAP3_cleaner)
-rm(GAP4_cleaner)
-gc()
 
-###############################################################################
-#   Removing Nuisance Areas from PAD Data                                   ####
-#      Prepping Layers                                                      ####
+dif1 <- st_erase(p1, p2)
 
-# Combined Roads 
-sf_use_s2(F)
-print(Sys.time())
-roads_combined <- st_read("1.Data/data_clean/RoadLayer/road_buffers.shp") %>% 
-  st_combine() %>% 
-  st_make_valid()
-print(Sys.time())
+p1 <- st_make_valid(GAP1) 
+p2 <- st_make_valid(GAP3)
 
-#      Cropping Roads                                                       ####
-print(Sys.time())
-GAP_clean_v2 <- GAP_clean %>% st_difference(roads_combined)
-beepr::beep(sound = 3)
-print(Sys.time())
+st_make_valid(spydf_states)
+
+mapview(GAP1, col.regions = "red") + 
+  mapview(GAP2,col.regions = "green") + 
+  mapview(GAP3, col.regions = "purple") +
+  mapview(GAP4, col.regions = "yellow")
+
+
+
+mapview(poly1) + mapview(poly2)
+
+GAP1_combined <- GAP1 %>% st_combine()
+GAP2_combined <- GAP2 %>% st_combine()
+GAP3_combined <- GAP3 %>% st_combine()
+GAP4_combined <- GAP4 %>% st_combine()
+
+poly1 <- GAP1 %>% st_combine()
+poly2 <- GAP2 
+
+gs <- st_difference(poly2,poly1)
+
+sf_use_s2(FALSE)
+GAP2_clean <- st_difference(GAP2,GAP1_combined)
+GAP3_clean <- st_difference(GAP3,GAP2_combined)
+GAP4_clean <- st_difference(GAP4,GAP3_combined)
+
+
+
+
+t <- protected_areas %>% 
+  filter(GAP_Sts %in% as.character(2:4))
+
+
+mapview(t) + mapview(GAP1, col.region = "red")+mapview(protected_areas)
+
+##############################################################################
+#  Exporting Maps                                                           ####
+
+
+
+
+
+
+
 ###############################################################################
