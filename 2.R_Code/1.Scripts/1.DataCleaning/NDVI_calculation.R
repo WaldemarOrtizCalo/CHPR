4d32b1271ec2ebe194f47fc25b521f8ac06f9136
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-01-09 00:36:24 -0700

    Cleaning script.

--- 2.R_Code/1.Scripts/1.DataCleaning/NDVI_calculation.R
+++ 2.R_Code/1.Scripts/1.DataCleaning/NDVI_calculation.R
@@ -1,5 +1,4 @@
-################################################################################                                                        ####
-# Script Details    
+#   Script Details                                                          ####
 
 # Author: Waldemar Ortiz-Calo
 
@@ -49,13 +48,18 @@ ndvi_names <- list.files("1.Data/data_raw/NDVI_Earthdata",
   str_remove("A") %>% unique()
 
 ###############################################################################
+#   NDVI Calculation and Export                                             ####
 
+# Export Directory
 export_dir <- "1.Data/data_clean/NDVI_MODIS"
+
 for (i in 1:length(ndvi_names)) {
   
+  # Making a List of Rasters 
   raster_list <- str_subset(ndvi_list,
                             pattern = ndvi_names[[i]])
   
+  # Creating the NIR layers
   R_NIR_1 <- ndvi_list[1] %>% rast() %>% .[[5]]
   R_red_1 <- ndvi_list[1] %>% rast() %>% .[[4]]
   R_NIR_2 <- ndvi_list[2] %>% rast() %>% .[[5]]
@@ -63,15 +67,26 @@ for (i in 1:length(ndvi_names)) {
   R_NIR_3 <- ndvi_list[3] %>% rast() %>% .[[5]]
   R_red_3 <- ndvi_list[3] %>% rast() %>% .[[4]]
   
+  # Calculating NDVI
   ndvi_1 <- (R_NIR_1 - R_red_1)/(R_NIR_1 + R_red_1)
   ndvi_2 <- (R_NIR_2 - R_red_2)/(R_NIR_2 + R_red_2)
   ndvi_3 <- (R_NIR_3 - R_red_3)/(R_NIR_3 + R_red_3)
   
+  # Creating final NDVI layer
   ndvi_final <- merge(ndvi_1,ndvi_2,ndvi_3) %>% 
-    project(shp_montana) %>% 
-    mask(shp_montana)
+    project("EPSG:4326") %>% 
+    mask(shp_montana) %>% 
+    crop(shp_montana)
   
+  # Exporting Raster
   writeRaster(ndvi_final,
               filename = paste0(export_dir,"/ndvi_",date2character(ndvi_names[[i]]),
-                                ".tif"))
+                                ".tif"),
+              overwrite = T)
+  
+  # Iteration Tracker
+  print(paste0(i, " out of ", length(ndvi_names), " completed"))
+  
 }
+
+###############################################################################
